5bc530dcc372f2fe2cc260235f5f1f283c86677d
Maksim <mksmvnv@icloud.com>
2024-08-22 09:33:53 +0300

    Add task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+class Task(models.Model):
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "task"
+        verbose_name_plural = "tasks"
+
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2056)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
