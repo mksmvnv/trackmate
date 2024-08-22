1ae2d22a24249b1907cacfa8b3775e7472be9db6
Maksim <mksmvnv@icloud.com>
2024-08-22 09:34:14 +0300

    Add index list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import Task
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "index.html"
+
+    model = Task
+    queryset = Task.objects.all()
