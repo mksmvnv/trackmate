e8672b74954a566688256393006ed8457bb983d7
Maksim <mksmvnv@icloud.com>
2024-08-24 01:47:06 +0300

    Combine create and list view classes into one and add update task status view class

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,37 @@
-from django.views.generic import ListView
+from django.forms import BaseModelForm
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import ListView, CreateView, UpdateView
 
 from .models import Task
+from .forms import TaskForm
 
 
-class IndexView(ListView):
+class CreateTaskListView(CreateView, ListView):
+    """Add and list tasks"""
+
     template_name = "index.html"
 
     model = Task
-    queryset = Task.objects.all()
+    form_class = TaskForm
+    success_url = "/"
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        return super().form_valid(form)
+
+    def form_invalid(self, form: BaseModelForm) -> HttpResponse:
+        return super().form_invalid(form)
+
+
+class UpdateTaskStatusView(UpdateView):
+    """Update task status"""
+
+    model = Task
+    fields = ["status"]
+
+    def form_valid(self, form: BaseModelForm) -> JsonResponse:
+        form.instance.status = self.request.POST.get("status") == "true"
+        self.object = form.save()
+        return JsonResponse({"success": True})
+
+    def form_invalid(self, form: BaseModelForm) -> JsonResponse:
+        return JsonResponse({"success": False, "errors": form.errors}, status=400)
