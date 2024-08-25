00ca1d1607b15c5e034d649cc65ba4454907f004
Maksim <mksmvnv@icloud.com>
2024-08-25 13:40:12 +0300

    Add update and delete urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import CreateTaskListView, UpdateTaskStatusView
+from . import views
 
 urlpatterns = [
-    path("", CreateTaskListView.as_view(), name="index"),
+    path("", views.CreateTaskListView.as_view(), name="index"),
     path(
-        "update_status/<int:pk>/", UpdateTaskStatusView.as_view(), name="update_status"
+        "update_status/<int:pk>/",
+        views.UpdateTaskStatusView.as_view(),
+        name="task-update-status",
     ),
+    path("update/<int:pk>/", views.UpdateTaskView.as_view(), name="task-update"),
+    path("delete/<int:pk>/", views.DeleteTaskView.as_view(), name="task-delete"),
 ]
