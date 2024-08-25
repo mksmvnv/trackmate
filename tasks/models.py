7182dc8a9b845257c64a242eba96dae95fd6a022
Maksim <mksmvnv@icloud.com>
2024-08-25 13:37:50 +0300

    Add datetime field

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Task(models.Model):
     title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2056)
+    description = models.TextField(max_length=2056, null=True, blank=True)
     status = models.BooleanField(default=False)
+    сreated_at = models.DateTimeField(default=now)
 
     class Meta:
         db_table = "tasks"
@@ -12,4 +14,4 @@ class Task(models.Model):
         verbose_name_plural = "tasks"
 
     def __str__(self):
-        return self.title
+        return f"{self.title} <{self.сreated_at.strftime('%d-%m-%Y %H:%M')}>"
