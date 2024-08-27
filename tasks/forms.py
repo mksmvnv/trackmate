87494009f5ea6305081b763f3766502b0fe17a80
Maksim <mksmvnv@icloud.com>
2024-08-27 19:18:45 +0300

    Add labels and update widget attrs

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,12 +6,20 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description"]
+        labels = {"title": "Название", "description": "Описание"}
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "width: 100%; height: 40px;",
+                    "placeholder": "Название задачи",
+                }
+            ),
             "description": forms.Textarea(
                 attrs={
                     "class": "form-control",
-                    "style": "width: 100%; height: 50px;",
+                    "style": "width: 100%; height: 60px;",
+                    "placeholder": "Описание задачи",
                 }
             ),
         }
