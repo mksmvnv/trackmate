0301533438925f1b2755b1a659fe43963e3af2e4
Maksim <mksmvnv@icloud.com>
2024-08-28 18:05:11 +0300

    Implement custom user creation form

---
+++ users/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class UserCreationForm(UserCreationForm):
+    usable_password = None
+
+    email = forms.EmailField(
+        label="Email",
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={
+                "autocomplete": "email",
+                "class": "form-control",
+                "placeholder": "Введите email",
+            }
+        ),
+    )
+
+    username = forms.CharField(
+        label="Логин",
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={
+                "autocomplete": "username",
+                "class": "form-control",
+                "placeholder": "Введите логин",
+            }
+        ),
+    )
+
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control",
+                "placeholder": "Введите пароль",
+            }
+        ),
+    )
+
+    password2 = forms.CharField(
+        label="Повторите пароль",
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control",
+                "placeholder": "Подтвердите пароль",
+            }
+        ),
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email")
