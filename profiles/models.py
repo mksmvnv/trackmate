9c954917904af17009bf2c0915ae635e37b3d74c
Maksim <mksmvnv@icloud.com>
2024-09-02 13:40:23 +0300

    Add temporary profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
+    )
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
