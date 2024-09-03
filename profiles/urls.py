0def010a6e2c1a0b5dce48b0591982737c6a3a2a
Maksim <mksmvnv@icloud.com>
2024-09-03 19:15:45 +0300

    Add edit profile url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("profiles/<int:id>/", views.ProfileView.as_view(), name="profile"),
+    path(
+        "profiles/edit/<int:id>/", views.EditProfileView.as_view(), name="edit-profile"
+    ),
 ]
