31200f6e29e6f647f96f70d5b1c94c4603e8535f
Maksim <mksmvnv@icloud.com>
2024-09-03 19:14:44 +0300

    Add new data for model and avatar upload path func

--- profiles/models.py
+++ profiles/models.py
@@ -3,11 +3,22 @@ from django.db import models
 from django.conf import settings
 
 
+def avatar_upload_path(instance, filename):
+    return f"profile_images/user_{instance.user.id}/{filename}"
+
+
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
     )
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(
+        default="profile_images/default.jpg", upload_to=avatar_upload_path
+    )
+    first_name = models.CharField(max_length=128, blank=True, null=True)
+    last_name = models.CharField(max_length=128, blank=True, null=True)
+    gender = models.CharField(max_length=128, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.user.username
