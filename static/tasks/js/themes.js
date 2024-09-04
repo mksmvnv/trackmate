8050c376ccaa937893812ff694ec9c8065051d91
Maksim <mksmvnv@icloud.com>
2024-09-04 04:47:29 +0300

    Fix some bugs

--- static/tasks/js/themes.js
+++ static/tasks/js/themes.js
@@ -1,14 +1,12 @@
 const htmlElement = document.getElementById('html-theme');
 const themeIcon = document.getElementById('theme-icon');
 const themeToggleBtn = document.getElementById('theme-toggle');
-const addTaskButton = document.getElementById('add-task-button');
+const submitButton = document.getElementById('submit-button');
 
 
 function setTheme(themeName) {
     htmlElement.setAttribute('data-bs-theme', themeName);
     localStorage.setItem('theme', themeName);
-
-    const taskButton = document.getElementById('task-button');
 
     // Change icon
 
@@ -20,11 +18,11 @@ function setTheme(themeName) {
 
     // Change button
 
-    if (taskButton) {
+    if (submitButton) {
         if (themeName === 'dark') {
-            taskButton.classList.replace('btn-dark', 'btn-secondary');
+            submitButton.classList.replace('btn-dark', 'btn-secondary');
         } else {
-            taskButton.classList.replace('btn-secondary', 'btn-dark');
+            submitButton.classList.replace('btn-secondary', 'btn-dark');
         }
     }
 }
