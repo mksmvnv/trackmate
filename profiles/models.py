dc4f185cf635af31a8794a53280e61c380094eb7
Maksim <mksmvnv@icloud.com>
2024-09-04 04:45:12 +0300

    Add new fields to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -3,22 +3,37 @@ from django.db import models
 from django.conf import settings
 
 
-def avatar_upload_path(instance, filename):
+def image_upload_path(instance, filename):
     return f"profile_images/user_{instance.user.id}/{filename}"
 
 
 class Profile(models.Model):
+    GENDER_CHOICES = [
+        ("М", "Мужской"),
+        ("Ж", "Женский"),
+        ("Д", "Другое"),
+    ]
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
     )
-    bio = models.TextField(blank=True, null=True)
-    avatar = models.ImageField(
-        default="profile_images/default.jpg", upload_to=avatar_upload_path
+    image = models.ImageField(
+        default="profile_images/default.jpg", upload_to=image_upload_path
     )
     first_name = models.CharField(max_length=128, blank=True, null=True)
     last_name = models.CharField(max_length=128, blank=True, null=True)
-    gender = models.CharField(max_length=128, blank=True, null=True)
+    profession = models.CharField(max_length=128, blank=True, null=True)
+    location = models.CharField(max_length=128, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=1, choices=GENDER_CHOICES, blank=True, null=True
+    )
     age = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = "profiles"
+        verbose_name = "profile"
+        verbose_name_plural = "profiles"
 
     def __str__(self):
         return self.user.username
