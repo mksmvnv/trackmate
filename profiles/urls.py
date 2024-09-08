2f6cfd2b0b8a3ca0d5b6b53eef11476c99c80699
Maksim <mksmvnv@icloud.com>
2024-09-08 20:06:09 +0300

    Add api url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from . import api_views
 
 urlpatterns = [
     path("profiles/<int:id>/", views.ProfileView.as_view(), name="profile"),
@@ -8,5 +9,10 @@ urlpatterns = [
         "profiles/<int:id>/update/",
         views.UpdateProfileView.as_view(),
         name="profile-update",
+    ),
+    path(
+        "api/task-status/<int:user_id>/",
+        api_views.TaskStatusDataView.as_view(),
+        name="task-api-status",
     ),
 ]
