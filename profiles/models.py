035bca7bf10b721fc9c7da9af9589727b488e957
Maksim <mksmvnv@icloud.com>
2024-09-08 20:02:25 +0300

    Implement test profile image manager and update image field

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,9 @@
-from django.db import models
+import os
 
+from django.db import models
 from django.conf import settings
 
-
-def image_upload_path(instance, filename):
-    return f"profile_images/user_{instance.user.id}/{filename}"
+from profiles.utils import FilePathManager, ImageGenerator
 
 
 class Profile(models.Model):
@@ -18,7 +17,9 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
     )
     image = models.ImageField(
-        default="profile_images/default.jpg", upload_to=image_upload_path
+        upload_to=FilePathManager("profile_images/").get_upload_path,
+        blank=True,
+        null=True,
     )
     first_name = models.CharField(max_length=128, blank=True, null=True)
     last_name = models.CharField(max_length=128, blank=True, null=True)
@@ -37,3 +38,45 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        default_image_path = (
+            f"profile_images/default_profile_image_user_{self.user.id}.png"
+        )
+
+        if not self.image or self.image.name == default_image_path:
+            self.image = ImageGenerator().generate_image(self)
+
+        super().save(*args, **kwargs)
+
+
+class ProfileImageManager:
+    def __init__(self, instance):
+        self.instance = instance
+        self.default_image_path = (
+            f"profile_images/default_profile_image_user_{self.instance.user.id}.png"
+        )
+
+    def get_old_image(self) -> str:
+        try:
+            return Profile.objects.get(pk=self.instance.pk).image
+        except Profile.DoesNotExist:
+            return None
+
+    def is_default_image(self, image_path) -> bool:
+        return image_path == os.path.join(settings.MEDIA_ROOT, self.default_image_path)
+
+    def remove_old_image(self) -> None:
+        old_image = self.get_old_image()
+        new_image = self.instance.image
+
+        if old_image and old_image != new_image:
+            old_image_path = old_image.path
+
+            if not self.is_default_image(old_image_path):
+                if os.path.exists(old_image_path):
+                    os.remove(old_image_path)
+
+    def set_default_image_if_needed(self) -> None:
+        if not self.instance.image:
+            self.instance.image = self.default_image_path
