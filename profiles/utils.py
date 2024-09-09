66a741fa7a65c71654fe732bc2c3c07433a47e63
Maksim <mksmvnv@icloud.com>
2024-09-09 20:03:56 +0300

    Delete file path manager and upgrade file path processor

--- profiles/utils.py
+++ profiles/utils.py
@@ -1,5 +1,6 @@
 import os
 
+from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.utils.deconstruct import deconstructible
 
@@ -11,33 +12,10 @@ class FilePathProcessor:
     def __init__(self, path: str) -> None:
         self.path = path
 
-    def __call__(self, instance: object, filename: str) -> str:
+    def __call__(self, instance, filename) -> str:
         ext = filename.split(".")[-1]
         filename = f"profile_image_user_{instance.user.id}.{ext}"
-        user_folder = os.path.join(self.path, f"user_{instance.user.id}")
-        full_path = os.path.join(user_folder, filename)
-
-        default_image_path = (
-            f"profile_images/default_profile_image_user_{instance.user.id}.png"
-        )
-
-        if not os.path.exists(user_folder):
-            os.makedirs(user_folder)
-
-        if os.path.exists(full_path) and full_path != os.path.join(
-            "media", default_image_path
-        ):
-            os.remove(full_path)
-
-        return full_path
-
-
-class FilePathManager:
-    def __init__(self, base_path: str):
-        self.processor = FilePathProcessor(base_path)
-
-    def get_upload_path(self, instance: object, filename: str) -> str:
-        return self.processor(instance, filename)
+        return os.path.join(self.path, f"user_{instance.user.id}", filename)
 
 
 class ImageGenerator:
@@ -51,11 +29,11 @@ class ImageGenerator:
         self.text_color = text_color
         self.background_color = background_color
 
-    def generate_image(self, instance):
+    def generate_image(self, instance) -> str:
         first_letter = instance.user.username[0].upper()
-        img_size = (500, 500)
-        img = Image.new("RGB", img_size, color=self.background_color)
-        draw = ImageDraw.Draw(img)
+        image_size = (500, 500)
+        image = Image.new("RGB", image_size, color=self.background_color)
+        draw = ImageDraw.Draw(image)
 
         font_path = finders.find("fonts/kode_mono/KodeMono-SemiBold.ttf")
 
@@ -70,19 +48,20 @@ class ImageGenerator:
 
         vertical_offset = 90
         text_position = (
-            (img_size[0] - text_width) / 2,
-            (img_size[1] - text_height) / 2 - vertical_offset,
+            (image_size[0] - text_width) / 2,
+            (image_size[1] - text_height) / 2 - vertical_offset,
         )
 
         draw.text(text_position, first_letter, font=font, fill=self.text_color)
 
-        image_path = f"profile_images/default_profile_image_user_{instance.user.id}.png"
-        full_path = os.path.join("media", image_path)
+        image_path = f"profile_images/default/user_{instance.user.id}/default_profile_image_user_{instance.user.id}.png"
+        full_path = os.path.join(settings.MEDIA_ROOT, image_path)
 
         user_folder = os.path.dirname(full_path)
-        if not os.path.exists(user_folder):
-            os.makedirs(user_folder)
 
-        img.save(full_path)
+        if not os.path.exists(user_folder):
+            os.makedirs(user_folder, exist_ok=True)
+
+        image.save(full_path)
 
         return image_path
