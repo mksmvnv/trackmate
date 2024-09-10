e3b8fcb35f09de9ce2971063762a3212c19d86ab
Maksim <mksmvnv@icloud.com>
2024-09-10 19:55:00 +0300

    Add set language url and update patterns to i18n type

--- trackmate/urls.py
+++ trackmate/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+from tasks import views
+
+urlpatterns = i18n_patterns(
+    # General urls
     path("admin/", admin.site.urls),
     path("", include("tasks.urls")),
     path("", include("users.urls")),
     path("", include("profiles.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Set language
+    path("set_language/", views.SetLanguageView.as_view(), name="set-language"),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
