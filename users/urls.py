f834a6f8d09984e8f4e10c4c17270abf0fe4ac48
Maksim <mksmvnv@icloud.com>
2024-09-22 21:40:51 +0300

    Add custom urls

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,19 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
+    # Custom urls
+    path("login/", views.CustomLoginView.as_view(), name="login"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    path(
+        "password_reset/",
+        views.CustomPasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    # Base urls
+    path("", include("django.contrib.auth.urls")),
 ]
