22e0ae55d6b377acab79886a0bf65e7cd159f177
Maksim <mksmvnv@icloud.com>
2024-10-15 20:01:46 +0300

    Add custom dark mode to pagination

--- src/static/tasks/js/themes.js
+++ src/static/tasks/js/themes.js
@@ -1,7 +1,9 @@
+// Dark mode
+
 const htmlElement = document.getElementById('html-theme');
 const themeIcon = document.getElementById('theme-icon');
 const themeToggleBtn = document.getElementById('theme-toggle');
-const submitButton = document.getElementById('submit-button');
+const themePaginations = document.querySelectorAll('.theme-pagination');
 
 
 function setTheme(themeName) {
@@ -15,6 +17,16 @@ function setTheme(themeName) {
     } else {
         themeIcon.classList.replace('bi-moon', 'bi-brightness-high');
     }
+
+    // Change pagination color
+
+    themePaginations.forEach(themePagination => {
+        if (themeName === 'dark') {
+            themePagination.style.color = '#fff';
+        } else {
+            themePagination.style.color = '#000';
+        }
+    });
 }
 
 const savedTheme = localStorage.getItem('theme') || 'light';
