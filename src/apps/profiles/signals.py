ebc4866231681c39b51e030fdede82a7af2ba1b0
Maksim <mksmvnv@icloud.com>
2024-10-17 17:10:11 +0300

    Add created user exception

--- src/apps/profiles/signals.py
+++ src/apps/profiles/signals.py
@@ -1,16 +1,27 @@
+import logging
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from apps.users.models import User
 from apps.profiles.models import Profile
 
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        try:
+            if not hasattr(instance, "profile"):
+                Profile.objects.create(user=instance)
+        except Exception as e:
+            logger.error(f"Error creating profile for user {instance.id}: {e}")
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except Profile.DoesNotExist:
+        logger.error(f"Profile for user {instance.id} does not exist")
